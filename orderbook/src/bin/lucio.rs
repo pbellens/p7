9fb775d84014cc0b27750ab4e0b762041cbdb470
pbellens <pbellens@gmail.com>
2022-06-28 18:31:57 +0200

    Update lucio.rs

--- orderbook/src/bin/lucio.rs
+++ orderbook/src/bin/lucio.rs
@@ -1,10 +1,11 @@
 extern crate orderbook;
 use orderbook::orderbook as ob;
+use orderbook::snapshot;
 use orderbook::data::orders;
 use futures::prelude::*;
 use tokio::net::TcpListener;
 use serde_json::Value;
-use tokio_util::codec::{FramedRead, LengthDelimitedCodec};
+use tokio_util::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
 use tokio_serde::formats::SymmetricalJson;
 
 
@@ -17,16 +18,24 @@ async fn main()
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
+        let (read, write) = socket.into_split();
 
-        let length_delimited = FramedRead::new(socket, LengthDelimitedCodec::new());
-        let mut deserialized = tokio_serde::SymmetricallyFramed::new(length_delimited, SymmetricalJson::<Value>::default()); 
+        let length_delimited_read = FramedRead::new(read, LengthDelimitedCodec::new());
+        let mut deserialized = tokio_serde::SymmetricallyFramed::new(length_delimited_read, SymmetricalJson::<Value>::default()); 
+        let length_delimited_write = FramedWrite::new(write, LengthDelimitedCodec::new());
+        let mut serialized = tokio_serde::SymmetricallyFramed::new(length_delimited_write, SymmetricalJson::<Value>::default()); 
 
          tokio::spawn(async move {
             let mut book = ob::OrderBook::new();
-            while let Some(msg) = deserialized.try_next().await.unwrap() {
+            while let Some(msg) = deserialized.try_next().await.unwrap() 
+            {
                 let order: orders::Order = serde_json::from_value(msg).unwrap();
                 println!("got order {}", order);
+
                 book.execute(order);
+                
+                let s  = serde_json::to_value(snapshot::Snapshot::new(&book, 4)).unwrap();
+                serialized.send(s).await.unwrap();
             }
         });
     }
