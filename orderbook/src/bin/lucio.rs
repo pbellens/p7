536122ff9f3236fb304e876ed145af2ba9bc41e3
pbellens <pbellens@gmail.com>
2022-07-01 16:57:41 +0200

    Update lucio.rs

--- orderbook/src/bin/lucio.rs
+++ orderbook/src/bin/lucio.rs
@@ -5,6 +5,7 @@ use orderbook::snapshot;
 use futures::prelude::*;
 use tokio::net::TcpListener;
 use serde_json::Value;
+use std::sync::{Arc, Mutex};
 use tokio_util::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
 use tokio_serde::formats::SymmetricalJson;
 
@@ -14,11 +15,14 @@ async fn main()
 {
     let listener = TcpListener::bind("127.0.0.1:17653").await.unwrap();
     println!("listening on {:?}", listener.local_addr());
+    let motherbook = Arc::new(Mutex::new(ob::OrderBook::new()));
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
         println!("got new connection!");
         let (read, write) = socket.into_split();
+
+        let book = Arc::clone(&motherbook);
 
         let length_delimited_read = FramedRead::new(read, LengthDelimitedCodec::new());
         let mut deserialized = tokio_serde::SymmetricallyFramed::new(length_delimited_read, SymmetricalJson::<Value>::default()); 
@@ -26,7 +30,6 @@ async fn main()
         let mut serialized = tokio_serde::SymmetricallyFramed::new(length_delimited_write, SymmetricalJson::<Value>::default()); 
 
          tokio::spawn(async move {
-            let mut book = ob::OrderBook::new();
             while let Some(msg) = deserialized.try_next().await.unwrap() 
             {
                 //let order: orders::Order = serde_json::from_value(msg).unwrap();
@@ -35,14 +38,19 @@ async fn main()
                     Ok(cmd) => match cmd {
                         commands::Cmd::Order(o) => 
                         { 
+                            let mut ob = book.lock().unwrap();
                             println!("got order {:?}", o);
-                            book.execute(o); 
+                            ob.execute(o); 
                         },
                         commands::Cmd::Snapshot(depth) => 
                         {
                             println!("got snapshot request for {}", depth);
-                            let s  = serde_json::to_value(snapshot::Snapshot::new(&book, depth)).unwrap();
-                            serialized.send(s).await.unwrap();
+                            let json = 
+                            {
+                                let ob = book.lock().unwrap();
+                                serde_json::to_value(snapshot::Snapshot::new(&*ob, depth)).unwrap()
+                            };
+                            serialized.send(json).await.unwrap();
                         }
                     },
                     Err(e) => println!("{}", e)
