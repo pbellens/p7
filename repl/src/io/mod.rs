766e722554549fd8b766ceb40768c88242488b03
pbellens <pbellens@gmail.com>
2022-07-05 15:48:22 +0000

    Update mod.rs

--- repl/src/io/mod.rs
+++ repl/src/io/mod.rs
@@ -1,13 +1,28 @@
 use std::net::SocketAddr;
 use orderbook::commands;
+use std::fmt::Display;
 
 pub mod handler;
 
 #[derive(Debug)]
 pub enum IoEvent {
-    Reply(anyhow::Result<String>),
     Connect(SocketAddr), 
     ConnectCheck, 
     Disconnect,
     Req(commands::Cmd)
+}
+
+#[derive(Debug)]
+pub enum IoReply {
+    Stum,
+    Reply(String)
+}
+
+impl Display for IoReply {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            IoReply::Stum => write!(f, ""),
+            IoReply::Reply(r) => write!(f, "{}", r),
+        }
+    }
 }
