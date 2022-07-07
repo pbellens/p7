d8cdee992ec63ff533c704e4e8666fc2f910a2c4
pbellens <pbellens@gmail.com>
2022-07-07 14:57:04 +0000

    Update market.rs

--- orderbook/src/matching/market.rs
+++ orderbook/src/matching/market.rs
@@ -11,15 +11,14 @@ pub fn market(ob: &mut ob::OrderBook, id: u32, side: Side, qty: u64) -> ob::Trad
             if let Some(_) = mi.pivot {
                 ob.asks.retain(|&_k, v| { ! v.is_empty() });
             } 
-            ob::Trade { fills, qty: qty - mi.remain }
+            ob::Trade { id: None, fills, qty: qty - mi.remain, remain: mi.remain }
         },
         Side::Sell => {
             let mi = core::gmatch(ob.bids.iter_mut().rev(), |lp, p| { lp > p }, id, qty, &mut fills, None);
             if let Some(_) = mi.pivot {
                 ob.bids.retain(|&_k, v| { ! v.is_empty() });
             } 
-            ob::Trade { fills, qty: qty - mi.remain }
+            ob::Trade { id: None, fills, qty: qty - mi.remain, remain: mi.remain }
         }
     }
 }
-
