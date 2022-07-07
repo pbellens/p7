75495c61f8320fda0d721394377b1bec0362e85a
pbellens <pbellens@gmail.com>
2022-07-07 14:56:28 +0000

    Update limit.rs

--- orderbook/src/matching/limit.rs
+++ orderbook/src/matching/limit.rs
@@ -1,4 +1,4 @@
-use crate::data::{fill::Fill, orders::Order, orders::OrderType, side::Side};
+use crate::data::{fill::Fill, orders::BookOrder, orders::OrderType, side::Side};
 use crate::orderbook as ob;
 use crate::matching::core;
 
@@ -6,6 +6,7 @@ use crate::matching::core;
 pub fn limit(ob: &mut ob::OrderBook, id: u32, side: Side, qty: u64, price: u64) -> ob::Trade 
 {
     let mut fills: Vec<Fill> = Vec::new();
+    let mut bookid: Option<u64> = None;
 
     match side {
         Side::Buy => {
@@ -15,13 +16,13 @@ pub fn limit(ob: &mut ob::OrderBook, id: u32, side: Side, qty: u64, price: u64) 
                 ob.bids
                     .entry(price)
                     .or_insert_with(|| Vec::with_capacity(queue_capacity))
-                    .push(Order {
+                    .push(BookOrder {
+                        id: ob.orderid,
                         qty: mi.remain,
-                        price,
-                        prod: 0,
-                        side: Side::Buy,
                         kind: OrderType::LimitOrder,
                     });
+                bookid = Some(ob.orderid);
+                ob.orderid += 1;
                 ob.max_bid = match ob.max_bid {
                     None => Some(price),
                     Some(b) if price > b => Some(price),
@@ -31,7 +32,7 @@ pub fn limit(ob: &mut ob::OrderBook, id: u32, side: Side, qty: u64, price: u64) 
             if let Some(_) = mi.pivot {
                 ob.asks.retain(|&_k, v| { ! v.is_empty() });
             } 
-            ob::Trade { fills, qty: qty - mi.remain }
+            ob::Trade { id: bookid, fills, qty: qty - mi.remain, remain: mi.remain }
         },
         Side::Sell => {
             let mi = core::gmatch(ob.bids.iter_mut().rev(), |lp, p| { lp > p }, id, qty, &mut fills, Some(price));
@@ -40,13 +41,13 @@ pub fn limit(ob: &mut ob::OrderBook, id: u32, side: Side, qty: u64, price: u64) 
                 ob.asks
                     .entry(price)
                     .or_insert_with(|| Vec::with_capacity(queue_capacity))
-                    .push(Order {
+                    .push(BookOrder {
+                        id: ob.orderid,
                         qty: mi.remain,
-                        price,
-                        prod: 0,
-                        side: Side::Sell,
                         kind: OrderType::LimitOrder,
                     });
+                bookid = Some(ob.orderid);
+                ob.orderid += 1;
                 ob.min_ask = match ob.min_ask {
                     None => Some(price),
                     Some(a) if price < a => Some(price),
@@ -56,7 +57,7 @@ pub fn limit(ob: &mut ob::OrderBook, id: u32, side: Side, qty: u64, price: u64) 
             if let Some(_) = mi.pivot {
                 ob.bids.retain(|&_k, v| { ! v.is_empty() });
             } 
-            ob::Trade { fills, qty: qty - mi.remain }
+            ob::Trade { id: bookid, fills, qty: qty - mi.remain, remain: mi.remain }
         }
     }
 }
